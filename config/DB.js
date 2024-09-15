bdd1b0bed4fe4d5d27a7426c18261e3acaba46e4
MANORANJAN SUTAR <149157746+Manoranjansutar@users.noreply.github.com>
2024-09-16 02:44:14 +0530

    Update DB.js

--- config/DB.js
+++ config/DB.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 export const connectDB = async () =>{
     try {
-        await mongoose.connect(process.env.MONGODB_url) 
+        await mongoose.connect(process.env.MONGODB_URL) 
         console.log("MongoDB connected")
     } catch (error) {
         console.log("MongoDB not connected",error)
